28b856fc75504f6c99c27a4ec8fd79b23f264c35
Jennifer Lê <jnnnle.sf@gmail.com>
2016-11-28 22:36:47 -0800

    GET

--- Server/server.js
+++ Server/server.js
@@ -6,6 +6,12 @@ import express from 'express';
 //create instance of our app (syntax is specific to express)
 const app = express();
 
+//REST - Express
+// GET it girl 
+app.get('/hi', (req, res) => {
+	res.send('Hi there Mr Gulbrandsen');
+});
+
 //start the server and set a port
 app.listen(1337);
 
