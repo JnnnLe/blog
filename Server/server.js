067070fd8b1fb6f70816780f83c5c0da967011a9
Jennifer Lê <jnnnle.sf@gmail.com>
2016-11-28 22:30:53 -0800

    Add es6 support + watch script

---
+++ Server/server.js
@@ -0,0 +1,11 @@
+//start the fun
+
+//import module express server
+import express from 'express';
+
+//create instance of our app (syntax is specific to express)
+const app = express();
+
+//start the server and set a port
+app.listen(1337);
+
